7d19095b35a463e10bd2c83cc559d0861b8aba77
LucasMezza <mezza.lucas12@gmail.com>
2024-04-24 19:00:52 -0300

    cartContext

---
+++ src/context/cart.jsx
@@ -0,0 +1,36 @@
+import { createContext, useReducer } from "react";
+
+export const CartContext = createContext();
+
+function useCartContext() {
+    const [state, dispatch] = useReducer(cartReducer, cartInitialState)
+
+    const addToCart = producto => dispatch({
+        type: 'ADD_TO_CART',
+        payload: producto
+    })
+
+    const removeFromCart = producto => dispatch({
+        type: 'REMOVE_FROM_CART',
+        payload: producto
+    })
+
+    const cleanCart = () => dispatch({ type: 'CLEAN_CART' })
+
+    return { state, addToCart, removeFromCart, cleanCart }
+} 
+
+export function CartProvider() {
+    const {state, addToCart, removeFromCart, cleanCart } = useCartContext()
+
+    return (
+        <CartContext.Provider value={{
+            cart: state,
+            addToCart,
+            removeFromCart,
+            cleanCart
+        }}>
+            {children}
+        </CartContext.Provider>
+    )
+}
