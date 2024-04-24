30743d78a84482505d2ee9371b9940c278c460ac
juaniB12 <bjuan.ignacio11@gmail.com>
2024-04-24 17:09:38 -0300

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import './App.css'
 import { useEffect } from 'react';
-import { initMercadoPago, Wallet } from '@mercadopago/sdk-react';
 
 function App() {
-  useEffect(() => {
-    initMercadoPago('', { locale: 'es-AR' });
-  }, []);
 
   return (
     <div>
-      <Wallet initialization={{preferenceId: ''}} />
+      
     </div>
   );
 }
